6904fd14e22922970e68b21f8727ea3c1f36a40c
YTCUSA02\2010 <wlu@yamatoamerica.com>
2024-05-24 16:39:35 -0700

    Description of the changes

--- app/wei/page.tsx
+++ app/wei/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
     return (
       <main className="flex min-h-screen flex-col p-6">
-        <h1 className="text-blue-900 font-bold dark:hover:text-2xl">I'm blue!</h1>
+        <h1 className="text-blue-900 font-bold dark:hover:text-2xl">Im blue!</h1>
       </main>
     );
   }
